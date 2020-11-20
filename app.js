e17a0ac4676ba05d4c17ae6cff390964defcc253
rcromer00 <cromerro@msu.edu>
2020-11-20 12:28:42 -0500

    Runs the application

--- app.js
+++ app.js
@@ -12,57 +12,80 @@ const render = require("./lib/htmlRenderer");
 
 var emp = [];
 
-
+console.log("Welcome to Team Generator!");
  var questions = inquirer.prompt([
+     //Engineer Questions
     {
        type: 'input',
-       message: 'What is your name?',
-       name: 'name', 
+       message: 'What is the engineer(s) name?',
+       name: 'engineerName', 
     },
     {
         type: 'input',
-        message: 'What is your email ',
-        name: 'email',
+        message: 'What is the engineer(s) email? ',
+        name: 'engineerEmail',
     },
     {
         type: 'input',
-        message: 'What is your id ',
-        name: 'id',
-    },
+        message: 'What is the engineer(s) id?',
+        name: 'engineerId',
+    }, 
     {
-        type: 'list',
-        message: 'What is your role?',
-        name: 'role',
-        choices: [
-            'Engineer',
-            'Manager',
-            'Intern'
-        ]
-    }, {
-            type: 'input',
-            message: 'Please enter your GitHub username.',
-            name: 'username',
-            when: (answers) => answers.role === 'Engineer'
-        },
-        {
-            type: 'input',
-            message: 'Please enter your Office Number.',
-            name: 'officeNumber',
-            when: (answers) => answers.role === 'Manager'
-        },
-        { 
-            type: 'input',
-            message: 'Please enter the name of your School.',
-            name: 'school',
-            when: (answers) => answers.role === 'Intern'
-        }
+    type: 'input',
+    message: 'Please enter the GitHub username of the engineer(s) .',
+    name: 'username',
+    },
+    //Manager Questions
+    {
+        type: 'input',
+        message: 'What is the manager(s) name?',
+        name: 'managerName', 
+     },
+     {
+         type: 'input',
+         message: 'What is the manager(s) email? ',
+         name: 'managerEmail',
+     },
+     {
+         type: 'input',
+         message: 'What is the manager(s) id?',
+         name: 'managerId',
+     }, 
+     {
+        type: 'input',
+        message: 'Please enter the manager(s) Office Number.',
+        name: 'officeNumber',
+    },
+    //Intern Questions
+    {
+        type: 'input',
+        message: 'What is the intern(s) name?',
+        name: 'internName', 
+     },
+     {
+         type: 'input',
+         message: 'What is the intern(s) email? ',
+         name: 'internEmail',
+     },
+     {
+         type: 'input',
+         message: 'What is the intern(s) id?',
+         name: 'internId',
+     }, 
+     { 
+        type: 'input',
+        message: 'Please enter the name of your School.',
+        name: 'school',
+    }
  ]).then(function(data){
      console.log(data);
-         const engineer = new Engineer(data.name,data.id, data.email, data.username);
+         const engineer = new Engineer(data.engineerName,data.engineerId, data.engineerEmail, data.username);
          emp.push(engineer);
-         const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+         const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.officeNumber);
          emp.push(manager);
-         const intern = new Intern(data.name, data.id, data.email, data.school);
+         const intern = new Intern(data.internName, data.internId, data.internEmail, data.school);
          emp.push(intern);
 
-         fs.writeFileSync(outputPath, render(emp),"utf-8");    
+         fs.writeFileSync(outputPath, render(emp),"utf-8");
+         console.log("Your file has been successfully written in the Output folder!");
+});
