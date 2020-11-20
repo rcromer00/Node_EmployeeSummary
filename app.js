29a1e6f1d9ccb72b6eba20428bbec5fe1dbe185d
rcromer00 <cromerro@msu.edu>
2020-11-19 23:57:52 -0500

    prompts user

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-// const gitHubAPI = require("github");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,33 +9,25 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { parse } = require("path");
-const { error } = require("console");
+
+var emp = [];
 
 
-// GitHub API
-// const github = new GitHubApi({
-//     // required
-//     version: "3.0.0",
-//     // optional
-//     debug: true,
-//     protocol: "https",
-//     host: "github.my-GHE-enabled-company.com",
-//     pathPrefix: "/api/v3", // for some GHEs
-//     timeout: 5000
-// });
-
-// Write code to use inquirer to gather information about the development team members,
- inquirer.prompt([
+ var questions = inquirer.prompt([
     {
        type: 'input',
        message: 'What is your name?',
-       name: 'name'
+       name: 'name', 
     },
     {
         type: 'input',
         message: 'What is your email ',
         name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your id ',
+        name: 'id',
     },
     {
         type: 'list',
@@ -59,41 +50,19 @@ const { error } = require("console");
             name: 'officeNumber',
             when: (answers) => answers.role === 'Manager'
         },
-        {
+        { 
             type: 'input',
             message: 'Please enter the name of your School.',
             name: 'school',
             when: (answers) => answers.role === 'Intern'
         }
- ])
- 
-//    function({
-//     github.user.getFollowingFromUser({
-//         user: " "
-//     }, function(err, res) {
-//         console.log(JSON.stringify(res));
-//       });
-//     });
-// }
+ ]).then(function(data){
+     console.log(data);
+         const engineer = new Engineer(data.name,data.id, data.email, data.username);
+         emp.push(engineer);
+         const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+         emp.push(manager);
+         const intern = new Intern(data.name, data.id, data.email, data.school);
+         emp.push(intern);
 
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+         fs.writeFileSync(outputPath, render(emp),"utf-8");    
